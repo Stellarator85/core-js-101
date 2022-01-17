aa368528432422116bf4c4d44a2a02e53eb60e8b
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2022-01-17 13:51:31 +0200

    feat: implement extractEmails-function

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -183,8 +183,9 @@ function convertToUpperCase(str) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+
+function extractEmails(str) {
+  return str.split(";");
 }
 
 /**
