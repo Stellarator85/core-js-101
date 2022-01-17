ac33c3e53845c7bc3834029c09523f40aa27076b
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2022-01-17 13:21:20 +0200

    feat: implement repeatString-function

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -115,8 +115,9 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'A', 5  => 'AAAAA'
  *   'cat', 3 => 'catcatcat'
  */
-function repeatString(/* value, count */) {
-  throw new Error('Not implemented');
+
+function repeatString(value, count) {
+  return value.repeat(count);
 }
 
 /**
