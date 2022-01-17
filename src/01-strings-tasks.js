176b71a03e8482be704dde81bb37869e09c1737d
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2022-01-17 13:08:17 +0200

    feat: implement getFirstChar-function

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -83,8 +83,9 @@ function extractNameFromTemplate(value) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+
+function getFirstChar(value) {
+  return value[0];
 }
 
 /**
