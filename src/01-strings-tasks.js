c1ac55ad880bda538d40a4a7d092c06abf9a77c3
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2022-01-17 12:49:05 +0200

    refactor: refactor code

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -67,10 +67,10 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
+
 function extractNameFromTemplate(value) {
   return value.slice(7,-1);
 }
-
 
 /**
  * Returns a first char of the given string.
