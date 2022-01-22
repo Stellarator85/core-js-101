a54fade85ba12106877d976fd6837b57d36d1f0c
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2022-01-22 14:29:47 +0200

    refactor: refactor code

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -329,18 +329,18 @@ function getPositivesCount(arr) {
  */
 function sortDigitNamesByNumericOrder(arr) {
   function wordToDigit(word) {
-    if (word === 'zero') return 0;
-    else if (word === 'one') return 1;
-    else if (word === 'two') return 2;
-    else if (word === 'three') return 3;
-    else if (word === 'four') return 4;
-    else if (word === 'five') return 5;
-    else if (word === 'six') return 6;
-    else if (word === 'seven') return 7;
-    else if (word === 'eight') return 8;
-    else if (word === 'nine') return 9;
-  };
-  return arr.map(x => wordToDigit(x));
+  if (word === 'zero') return 0;
+  else if (word === 'one') return 1;
+  else if (word === 'two') return 2;
+  else if (word === 'three') return 3;
+  else if (word === 'four') return 4;
+  else if (word === 'five') return 5;
+  else if (word === 'six') return 6;
+  else if (word === 'seven') return 7;
+  else if (word === 'eight') return 8;
+  else if (word === 'nine') return 9;
+  }
+  return arr.map((x) => wordToDigit(x));
 }
 
 /**
