6ca675c3bd95affd459d9b9b7df65df546d69866
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2022-01-22 14:22:59 +0200

    refactor: refactor code

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -333,11 +333,11 @@ function sortDigitNamesByNumericOrder(arr) {
     else if (word === 'one') return 1;
     else if (word === 'two') return 2;
     else if (word === 'three') return 3;
-    else if (word === 'four' return 4;
-    else if (word === 'five' return 5;
-    else if (word === 'six' return 6;
-    else if (word === 'seven' return 7;
-    else if (word === 'eight' return 8;
+    else if (word === 'four') return 4;
+    else if (word === 'five') return 5;
+    else if (word === 'six') return 6;
+    else if (word === 'seven') return 7;
+    else if (word === 'eight') return 8;
     else if (word === 'nine') return 9;
   };
   return arr.map(x => wordToDigit(x));
