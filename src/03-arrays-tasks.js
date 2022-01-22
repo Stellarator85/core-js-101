ca36cfe76e5c34402e6510fec41b16fcfb585e43
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2022-01-22 13:30:18 +0200

    refactor: refactor code

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -286,6 +286,13 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
+
+
+
+
+
+
+
 function get3TopItems(arr) {
   let arr1 = arr.sort((a, b) => b - a);
   arr1.length = 3;
