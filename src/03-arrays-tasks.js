3bf2d9175b5f2269330057029ec1477e20c0be0e
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2022-01-22 13:43:15 +0200

    refactor: refactor code

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -286,24 +286,16 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-
-
-
-
-
-
-
-
-
-
-
-
 function get3TopItems(arr) {
-  let arr1 = arr.sort((a, b) => b - a);
-  arr1.length = 3;
-  return arr1;
-}
+  function sorter(a, b) {
+    return a - b;
+  }
 
+  if (arr.length <= 3) {
+    return arr.sort(sorter).reverse();
+  }
+  return arr.sort(sorter).reverse().slice(0, 3);
+}
 
 /**
  * Returns the number of positive numbers from specified array
